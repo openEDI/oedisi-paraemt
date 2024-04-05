d3c15ce6d3fe8df8e830171435fdb531bfc9c98e
Min Xiong <minxiongutk@gmail.com>
2024-04-05 13:12:14 -0600

    Delete unnecessary files

--- models/2gen_psse/auto.py
+++
@@ -1,3 +0,0 @@
-# File:"C:\Users\bwang2\Documents\GitHub\emt\models\2gen_psse\auto.py", generated on TUE, FEB 22 2022  11:52, PSS(R)E release 34.04.00
-psspy.machine_chng_2(1,r"""1""",[_i,_i,_i,_i,_i,_i],[_f,_f,_f,_f,_f,_f,_f,0.0,_f,_f,_f,_f,_f,_f,_f,_f,_f])
-psspy.machine_chng_2(4,r"""1""",[_i,_i,_i,_i,_i,_i],[_f,_f,_f,_f,_f,_f,_f,0.0,_f,_f,_f,_f,_f,_f,_f,_f,_f])
